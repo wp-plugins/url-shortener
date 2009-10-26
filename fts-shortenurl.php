2efe6d341cdbef611e3b5414ad4f0815a257430a
geraldyeo <geraldyeo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-26 07:29:23 +0000

    git-svn-id: https://plugins.svn.wordpress.org/url-shortener/tags/1.4@167060 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fts-shortenurl.php
+++ fts-shortenurl.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: URL Shortener
-Plugin URI: http://fusedthought.com
+Plugin URI: http://fusedthought.com/downloads/url-shortener-wordpress-plugin/
 Description: This plugin integrates multiple URL Shortening service with your WordPress.org installation. Brings a similar functionality of WordPress.com's WP.me shortlinks feature but using 3rd partly URL Shorteners. 
 Author: Gerald Yeo (Fusedthought)
 Author URI: http://fusedthought.com
