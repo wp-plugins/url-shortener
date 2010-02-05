e714969f254b6d7b4e1b505780ec03ee6e7b3107
geraldyeo <geraldyeo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-05 08:42:49 +0000

    git-svn-id: https://plugins.svn.wordpress.org/url-shortener/tags/2.1@202119 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ fts-shortenurl-logic.php
@@ -0,0 +1,42 @@
+<?php
+$newshort = new FTShorten();
+$newshort->url = $posturl;
+$newshort->apiextend = $fts_urlfx['urlbetaservices'];
+switch($selectedservice){				
+	case 'supr':
+		$newshort->name = htmlentities($fts_urlfx['apiuser_supr'], ENT_QUOTES);
+		$newshort->apikey = htmlentities($fts_urlfx['apikey_supr'], ENT_QUOTES);
+		break;
+	case 'bitly':
+		$newshort->name = htmlentities($fts_urlfx['apiuser_bitly'], ENT_QUOTES);
+		$newshort->apikey = htmlentities($fts_urlfx['apikey_bitly'], ENT_QUOTES);
+		break;
+	case 'trim':
+		$newshort->name = htmlentities($fts_urlfx['apiuser_trim'], ENT_QUOTES);
+		$newshort->apikey = htmlentities($fts_urlfx['apikey_trim'], ENT_QUOTES);
+		break;
+	case 'snipurl':
+		$newshort->name = htmlentities($fts_urlfx['apiuser_snip'], ENT_QUOTES);
+		$newshort->apikey = htmlentities($fts_urlfx['apikey_snip'], ENT_QUOTES);
+		$selectedservice = $fts_urlfx['snipprefix'];
+		break;		
+	case 'cligs':
+		$newshort->apikey = htmlentities($fts_urlfx['apikey_cligs'], ENT_QUOTES);
+		break;		
+	case 'shortie':
+		$newshort->name = htmlentities($fts_urlfx['apiuser_shortie'], ENT_QUOTES);
+		$newshort->apikey = htmlentities($fts_urlfx['apikey_shortie'], ENT_QUOTES);
+		break;		
+	case 'pingfm':
+		$newshort->apikey = htmlentities($fts_urlfx['apikey_pingfm'], ENT_QUOTES);
+		$newshort->pingfmapi = 'f51e33510d3cbe2ff1e16a4a4897f099';
+		break;
+	case 'awesm':
+		$newshort->apikey =  htmlentities($fts_urlfx['apikey_awesm'], ENT_QUOTES);
+		break;	
+	default:
+		break;
+}
+$newshort->service = $selectedservice;
+$short = $newshort->shorturl();
+?>
---
+++ req/addtable.php
@@ -0,0 +1,78 @@
+<?php
+function fts_mass_generate(){
+	$plugin_url = WP_PLUGIN_URL.'/'.plugin_basename( dirname(dirname(__FILE__)) );
+	wp_enqueue_script('fts_surl_edit', $plugin_url.'/req/display/editcolumn.js',array('jquery'),1.0);
+	
+	
+	if(isset($_GET['btnurl']) || isset($_GET['btnurl2'])){
+		$ftsuri = basename($_SERVER["SCRIPT_FILENAME"]);
+		$bulkshort = $_GET['bulkurl'];
+		if ($bulkshort == '0'){
+			$bulkshort = $_GET['bulkurl2'];
+		}
+		$pid_array = $_GET['post'];
+		switch ($bulkshort){
+			case 'generate':
+				foreach( (array) $pid_array as $pid ) {
+					if($ftsuri == 'edit.php'){
+						fts_shortenurl($pid, 'post', false);
+					} elseif($ftsuri == 'edit-pages.php'){
+						fts_shortenurl($pid, 'page', false);
+					}
+				}
+				break;
+			case 'delete':
+				foreach( (array)  $pid_array as $pid ) {
+					delete_post_meta($pid , 'shorturl');
+					delete_post_meta($pid , 'short_url');
+				}
+				break;
+			default:
+				break;
+		}
+	}
+}
+function fts_edit_generate(){
+	$ftsuri = basename($_SERVER["SCRIPT_FILENAME"]);
+	$ftsgensurl = $_GET['urlid'];
+	if($ftsgensurl !='' && is_numeric($ftsgensurl)){
+		if($ftsuri == 'edit.php' ){
+			fts_shortenurl($ftsgensurl, 'post', false);
+		} elseif($ftsuri == 'edit-pages.php' ) {
+			fts_shortenurl($ftsgensurl, 'page', false);
+		}
+	}	
+}
+function add_urlshortner_column($columns){
+	$columns['Short_URL'] = __('Short URL');
+   	return $columns;
+}
+function manage_urlshortner_column($column_name, $id){
+	if ($column_name == "Short_URL"){
+		$short = get_post_meta($id, 'shorturl', true); 
+		if (!$short){
+			$short = get_post_meta($id, 'short_url', true);
+		}
+		if ($short){
+			print '<a class="ftsgt" href="'.$short.'"/>Generated</a>';	
+		} else {
+			$ftsuri = basename($_SERVER["SCRIPT_FILENAME"]);
+			if($ftsuri == 'edit.php' ){
+				print '<a class="button ftsgb" href="'.admin_url('edit.php').'?urlid='.$id.'">Generate Now</a>';
+			} elseif($ftsuri == 'edit-pages.php' ) {
+				print '<a class="button ftsgb" href="'.admin_url('edit-pages.php').'?urlid='.$id.'">Generate Now</a>';
+			}	
+		}
+	}
+}
+
+
+add_filter('manage_posts_columns', 'add_urlshortner_column');
+add_action('manage_posts_custom_column', 'manage_urlshortner_column', 10, 2);
+add_filter('manage_pages_columns', 'add_urlshortner_column');
+add_action('manage_pages_custom_column', 'manage_urlshortner_column', 10, 2);
+add_action('load-edit.php', 'fts_edit_generate');
+add_action('load-edit-pages.php', 'fts_edit_generate');
+add_action('load-edit.php', 'fts_mass_generate');
+add_action('load-edit-pages.php', 'fts_mass_generate');
+?>
